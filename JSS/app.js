5b7a190982c864c9dbbb7372cab47c3489d4ef8f
shavvvv <shavaungreen102@gmail.com>
2024-07-01 18:52:49 -0400

    Challenge 1

---
+++ JSS/app.js
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------
+Challenge: 00-sayHello (example)
+Difficulty: Basic
+Prompt:
+Write a function called sayHello that returns the string "Hello!".
+Examples:
+sayHello() //=> Hello!
+-----------------------------------------------------------------*/
+// Your solution for 00-sayHello (example) here:
+
+
+function sayHello() {
+    console.log("Hello!");
+};
+
+
+sayHello();
+
+
